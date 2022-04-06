2fc862d350a23bfb82fa6bcf96744a88ff1c39b4
willflemmer <>
2022-04-06 20:00:48 +0100

    add initialSetup.sh

---
+++ initialSetup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd app
+
+echo 'Installing dependencies...'
+yarn
+bundle
+
+echo 'Nice, now lets do the native dependencies 😳'
+yarn install:pods
+
+echo 'Cool, lets try build this thing 🤓'
+
+yarn react-native run-ios
+
+echo 'Awesome, almost there. Run the metro server and you should be cooking with gas:'
+echo 'yarn start'
