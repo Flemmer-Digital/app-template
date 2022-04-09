7313dd1c6b9b36ec31294ca513c96a9c1bc9a1f7
oliver flemmer <oli.flemmer@gmail.com>
2022-04-09 17:30:48 +0100

    Moved all scripts into scripts folder
    Create pull_request_template.md
    
    fix: shell scripts
    
    Revert "fix: shell scripts"
    
    This reverts commit c753549a519431200ece2e76604f093897048a99.
    
    Update pull_request_template.md
    
    add initial navigation
    
    add react native screens
    
    fix: unuathenticated navigation
    
    bump react-native-gesture-handler to 2.3.0
    
    sign-in-view
    
    signin test
    
    add packages and build
    
    absolute imports
    
    update useAuthenticator
    
    add sentry package
    
    work on authentication
    
    feat(tooling): add prettier pre commit
    
    Fix to abide by ESLint rules
    
    Add test component to app.tsx
    
    Add prettier to root
    
    Undo App.tsx
    
    update ruby version
    
    remove test component

--- README.md
+++ README.md
@@ -40,6 +40,15 @@ yarn createComp `componentName` - Creates files for component, styles and test i
 3. `sh initialSetup.sh` (might take a couple of attempts) 
 4. `yarn start`
 
+### Authentication
+
+### Sentry
+Create a new project in sentry.
+Set up in index.js (docs)[https://docs.sentry.io/platforms/react-native/]
+run
+`npx @sentry/wizard -i reactNative -p ios android`
+then run:
+`yarn install:pods`
 
 ## Building The App
 ### Ios
--- app/src/navigation/useAuthenticator.tsx
+++ app/src/navigation/useAuthenticator.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import { useApolloClient } from '@apollo/client';
+import * as Sentry from '@sentry/react-native';
+
+import authenticate from 'src/modules/authentication/authenticate';
+
+import registerDevice from 'src/modules/firebase/notifications/register';
+import sentrySetCurrentUser from 'src/components/utils/setCurrentUser';
+
 export interface Authenticator {
   authenticated: boolean;
   authenticating: boolean;
@@ -15,16 +23,50 @@ const defaultState = {
 
 const useAuthenticator = (): Authenticator => {
   const [state, setState] = React.useState(defaultState);
+  const apolloClient = useApolloClient()
 
-  const authenticatedWithState = React.useCallback(
-    (storedTokenOnly: boolean) => {},
-    [],
+  const authenticateWithState = React.useCallback(   
+    async (storedTokenOnly: boolean) => {
+    setState((prevState) => ({ ...prevState, authenticating: true }));
+
+    try {
+      const result = await authenticate({ storedTokenOnly });
+      setState((prevState) => ({ ...prevState, authenticating: false, authenticated: !!result }));
+
+      if (!result) return;
+
+      await sentrySetCurrentUser(apolloClient);
+      await registerDevice(apolloClient);
+    } catch (err: any) {
+      const errorString = err.toString();
+
+      // Ignore user cancellations, waiting on a better way to do this: https://github.com/FormidableLabs/react-native-app-auth/issues/565
+      if (
+        errorString === 'Error: User cancelled flow' ||
+        errorString ===
+          'Error: The operation couldn’t be completed. (org.openid.appauth.general error -3.)'
+      ) {
+        setState((prevState) => ({ ...prevState, authenticated: false, authenticating: false }));
+        return;
+      }
+
+      setState((prevState) => ({ ...prevState, error: err.toString(), authenticating: false }));
+      Sentry.captureException(err);
+    }
+  },
+  [apolloClient],
   );
 
+  React.useEffect(() => {
+    authenticateWithState(true);
+  }, [authenticateWithState]);
+
   return {
-    authenticate: () => authenticatedWithState(false),
+    authenticate: () => authenticateWithState(false),
     authenticated: state.authenticated,
     authenticating: state.authenticating,
     error: state.error,
   };
 };
+
+export default useAuthenticator
