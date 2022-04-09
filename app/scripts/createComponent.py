e59420fa294cbd04c3e3d0c138bddc56b3d94912
oliver flemmer <oli.flemmer@gmail.com>
2022-04-09 14:43:17 +0100

    Create Component file contents

--- app/scripts/createComponent.py
+++ app/scripts/createComponent.py
@@ -1,4 +1,15 @@
 import sys
 
 name = sys.argv[1]
+capitalName = name.capitalize()
+fileContents = f"""import React from 'react';
+import styles from './{name}Styles';
+
+const {capitalName}: React.FC = () => {{
+  return <></>;
+}};
+
+export default {capitalName};"""
+
 print(f"Creating Component: {name}")
+print(fileContents)
