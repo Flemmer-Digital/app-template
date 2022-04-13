7f38bd3824219ea47dc1394c0088c8d08b4879d5
willflemmer <>
2022-04-13 18:09:31 +0100

    update useAuthenticator

--- app/src/modules/authentication/authenticate.ts
+++ app/src/modules/authentication/authenticate.ts
@@ -1,9 +1,11 @@
 import {authorize} from 'react-native-app-auth';
 import config from './nativeOauthConfig';
 
-export const authenticate = async () => {
+const authenticate = async () => {
   console.log(authorize);
   const authState = await authorize(config);
 
   return authState;
 };
+
+export default authenticate
