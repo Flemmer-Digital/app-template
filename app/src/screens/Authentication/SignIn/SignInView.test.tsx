2799facc3828c4e63322cc84994343762d68834f
willflemmer <>
2022-04-13 08:27:22 +0100

    sign-in-view

---
+++ app/src/screens/Authentication/SignIn/SignInView.test.tsx
@@ -0,0 +1,9 @@
+import { render as testRender } from '@testing-library/react-native'
+import SignInView from './SignInView';
+import React from 'react';
+
+const render = () => testRender(<SignInView authenticator={{}} navigation={{}} />)
+it('renders and shows sign in text', () => {
+  const { getByText } = render()
+  expect(getByText('Sign In')).toBeTruthy();
+});
