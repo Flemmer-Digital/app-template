b3b6b2eec4891c785d06225b4703cf35ea359689
willflemmer <>
2022-04-14 08:03:19 +0100

    work on authentication

--- app/src/modules/authentication/authenticate.ts
+++ app/src/modules/authentication/authenticate.ts
@@ -1,11 +1,98 @@
-import {authorize} from 'react-native-app-auth';
-import config from './nativeOauthConfig';
+import { authorize, AuthConfiguration } from 'react-native-app-auth';
 
-const authenticate = async () => {
-  console.log(authorize);
-  const authState = await authorize(config);
+import RWLock from '../synchronisation/RWLock';
 
-  return authState;
+import { retrieveOauthToken, storeOauthToken } from './tokenStore';
+
+import NativeOauthToken from './NativeOauthToken';
+import nativeOauthConfig from './nativeOauthConfig';
+import { signOut } from './signout';
+
+let refreshing = false;
+const tokenLock = new RWLock();
+
+export class ExpiredAccessTokenException extends Error {}
+
+const requestAccessToken = async (config: AuthConfiguration) => {
+  const result = await authorize(config);
+
+  const token = new NativeOauthToken(result.accessToken, result.refreshToken);
+
+  await storeOauthToken(token);
 };
 
-export default authenticate
+const hasAccessToken = async (): Promise<boolean> => !!(await retrieveOauthToken());
+
+const refreshToken = async () => {
+  const token = await retrieveOauthToken();
+
+  if (!token) {
+    throw new Error('attempting to refresh but no token?');
+  }
+
+  try {
+    await token.refresh();
+  } catch (ex) {
+    if (ex.message && ex.message.includes('invalid_grant')) {
+      await signOut();
+    }
+
+    throw ex;
+  }
+};
+
+export const getAccessToken = async (): Promise<string> => {
+  await tokenLock.checkRead();
+
+  const token = await retrieveOauthToken();
+
+  if (!token) {
+    throw new Error('No access token');
+  }
+
+  return token.accessToken;
+};
+
+export const refreshAccessToken = async (): Promise<void> => {
+  if (!refreshing) {
+    refreshing = true;
+    await tokenLock.withWriteLock(async () => {
+      await refreshToken();
+    });
+    refreshing = false;
+  }
+};
+
+/**
+ * Calls the provided function with an access token.
+ * If the function raises an ExpiredAccessTokenException it will be retried once with a refreshed access token
+ */
+export const withAccessToken = async <T>(
+  callback: (accessToken: string) => Promise<T>,
+): Promise<T> => {
+  try {
+    return await callback(await getAccessToken());
+  } catch (ex) {
+    if (ex instanceof ExpiredAccessTokenException) {
+      await refreshAccessToken();
+      return callback(await getAccessToken());
+    }
+
+    throw ex;
+  }
+};
+
+interface AuthenticateArgs {
+  storedTokenOnly: boolean;
+}
+
+export const authenticate = async ({ storedTokenOnly }: AuthenticateArgs): Promise<boolean> => {
+  const hasToken = await hasAccessToken();
+
+  if (hasToken) return true;
+  if (storedTokenOnly) return false;
+
+  await requestAccessToken(await nativeOauthConfig());
+
+  return true;
+  
