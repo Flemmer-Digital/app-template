655345802799d988c12836a56f3aab2693ca25ae
oliver flemmer <oli.flemmer@gmail.com>
2022-04-14 21:55:49 +0100

    Undo App.tsx

--- app/src/components/test/test.test.tsx
+++
@@ -1,2 +0,0 @@
-import Test from './test'
-
--- app/src/components/test/testStyles.ts
+++
@@ -1,8 +0,0 @@
-import {StyleSheet, Dimensions} from 'react-native';
-const {width, height} = Dimensions.get('window');
-export const vh = height / 100;
-export const vw = width / 100;
-
-const testStyles = StyleSheet.create({});
-
-export default testStyles;
