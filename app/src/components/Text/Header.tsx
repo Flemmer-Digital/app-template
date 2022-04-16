fa67ef38c63fa7ab71f06272610a084cf37bf1d5
oliver flemmer <oli.flemmer@gmail.com>
2022-04-16 18:29:20 +0100

    Add color prop

--- app/src/components/Text/Header.tsx
+++ app/src/components/Text/Header.tsx
@@ -6,12 +6,15 @@ interface HeaderProps {
   style?: any;
   children?: any;
   variation?: 'h1' | 'h2' | 'h3' | 'mini';
+  color: string;
 }
 
-const Header: React.FC<HeaderProps> = ({ style, children, variation }) => {
+const Header: React.FC<HeaderProps> = ({ style, children, variation, color }) => {
   return (
     <>
-      <Text style={[styles.base, variation ? styles[variation] : null, style]}>{children}</Text>
+      <Text style={[styles.base, variation ? styles[variation] : null, { color: color }, style]}>
+        {children}
+      </Text>
     </>
   );
 };
