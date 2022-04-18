ec26063112ecb0768c6e9d3f6a382fc93fcc3195
oliver flemmer <oli.flemmer@gmail.com>
2022-04-18 16:33:39 +0100

    Add tests

--- app/src/components/loadingSpinner/loadingSpinner.test.tsx
+++ app/src/components/loadingSpinner/loadingSpinner.test.tsx
@@ -1,1 +1,15 @@
-import Loadingspinner from './loadingSpinnerView';
+import LoadingSpinner from './loadingSpinnerView';
+import { render as testRender } from '@testing-library/react-native';
+import React from 'react';
+
+const render = (props) => testRender(<LoadingSpinner testID="spinner" loading={props.loading} />);
+
+it('renders and shows sign in text', () => {
+  const { getByTestId } = render({ loading: false });
+  expect(getByTestId('spinner')).toBeTruthy();
+});
+
+it('dissapears when loading is true', () => {
+  const { queryByTestId } = render({ loading: true });
+  expect(queryByTestId('spinner')).toBeFalsy();
+});
