ddc0fc0c2dd9c0b561e65639e9fb1cced0e0baa5
oliver flemmer <oli.flemmer@gmail.com>
2022-04-18 16:04:43 +0100

    Add custom styles to container view

--- app/src/components/loadingSpinner/loadingSpinnerView.tsx
+++ app/src/components/loadingSpinner/loadingSpinnerView.tsx
@@ -6,14 +6,15 @@ interface LoadingspinnerProps {
   color?: string;
   size?: 'small' | 'large';
   fullscreen?: boolean;
+  style: any;
 }
 
-const Loadingspinner: React.FC<LoadingspinnerProps> = ({ color, size, fullscreen }) => {
+const Loadingspinner: React.FC<LoadingspinnerProps> = ({ color, size, fullscreen, style }) => {
   const fullscreenStyle = fullscreen ? styles.fullscreen : {};
   const fullScreenSpinner = fullscreen ? styles.fullScreenSpinner : {};
   return (
     <>
-      <View style={fullscreenStyle}>
+      <View style={[fullscreenStyle, style]}>
         <ActivityIndicator color={color} size={size} style={fullScreenSpinner} />
       </View>
     </>
