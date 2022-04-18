f42edcd75e8d238b81bbb819c47b560aa8bc0c8b
oliver flemmer <oli.flemmer@gmail.com>
2022-04-18 16:37:02 +0100

    pre comit test

--- app/src/components/loadingSpinner/loadingSpinner.test.tsx
+++ app/src/components/loadingSpinner/loadingSpinner.test.tsx
@@ -1,15 +1,16 @@
 import LoadingSpinner from './loadingSpinnerView';
-import { render as testRender } from '@testing-library/react-native';
+import {render as testRender} from '@testing-library/react-native';
 import React from 'react';
 
-const render = (props) => testRender(<LoadingSpinner testID="spinner" loading={props.loading} />);
+const render = (props: any) =>
+  testRender(<LoadingSpinner testID="spinner" loading={props.loading} />);
 
 it('renders and shows sign in text', () => {
-  const { getByTestId } = render({ loading: false });
+  const {getByTestId} = render({loading: false});
   expect(getByTestId('spinner')).toBeTruthy();
 });
 
 it('dissapears when loading is true', () => {
-  const { queryByTestId } = render({ loading: true });
+  const {queryByTestId} = render({loading: true});
   expect(queryByTestId('spinner')).toBeFalsy();
 });
