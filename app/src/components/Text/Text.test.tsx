8a5be91f12cef7a3d556556305f8a9088a040b98
oliver flemmer <oli.flemmer@gmail.com>
2022-04-18 15:27:40 +0100

    Update Text Tests

--- app/src/components/Text/Text.test.tsx
+++ app/src/components/Text/Text.test.tsx
@@ -1,28 +1,19 @@
 import React from 'react';
 import { render as testRender } from '@testing-library/react-native';
-import Text from './Text';
-import Header from './Header';
+import Text from './TextView';
 
-const render = () =>
-  testRender(
-    <Text>
-      <Header variation="h1">Header</Header>
-      <Text style={{ fontSize: 27 }}>Styled Text</Text>
-    </Text>,
-  );
+const render = () => testRender(<Text style={{ fontSize: 27 }}>Styled Text</Text>);
 
 describe('Text Component', () => {
   it('renders and shows text', () => {
     const { getByText } = render();
-    const header = getByText('Header');
     const styledText = getByText('Styled Text');
-    expect(header).toBeTruthy();
     expect(styledText).toBeTruthy();
   });
 
-  it('changes styles with style prop', () => {
+  it('Changes styles with style prop', () => {
     const { getByText } = render();
     const styledText = getByText('Styled Text');
-    expect(styledText.props.style[2].fontSize).toEqual(27);
+    expect(styledText.props.style.filter((item: any) => item?.fontSize === 27)).toBeTruthy();
   });
 });
