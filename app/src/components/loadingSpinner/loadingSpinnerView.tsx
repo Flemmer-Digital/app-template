3f6e3bc20086c216a8f6df32f0bd8f676a9dfbde
oliver flemmer <oli.flemmer@gmail.com>
2022-04-18 15:56:23 +0100

    Add Spinner component with styles and fullscreen

--- app/src/components/loadingSpinner/loadingSpinnerView.tsx
+++ app/src/components/loadingSpinner/loadingSpinnerView.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { ActivityIndicator, View } from 'react-native';
 import styles from './loadingSpinnerStyles';
 
-interface LoadingspinnerProps {}
+interface LoadingspinnerProps {
+  color?: string;
+  size?: number;
+  fullscreen?: boolean;
+}
 
-const Loadingspinner: React.FC<LoadingspinnerProps> = () => {
-  return <></>;
+const Loadingspinner: React.FC<LoadingspinnerProps> = ({ color, size, fullscreen }) => {
+  const fullscreenStyle = fullscreen ? styles.fullscreen : {};
+  const fullScreenSpinner = fullscreen ? styles.fullScreenSpinner : {};
+  return (
+    <>
+      <View style={fullscreenStyle}>
+        <ActivityIndicator color={color} size={size} style={fullScreenSpinner} />
+      </View>
+    </>
+  );
 };
 
 export default Loadingspinner;
