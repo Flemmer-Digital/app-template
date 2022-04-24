726a29e94451760f8eb210dc346b2594ab5f020e
oliver flemmer <oli.flemmer@gmail.com>
2022-04-24 12:07:02 +0100

    reword test and remove test for loading prop

--- app/src/components/loadingSpinner/loadingSpinner.test.tsx
+++ app/src/components/loadingSpinner/loadingSpinner.test.tsx
@@ -1,16 +1,10 @@
 import LoadingSpinner from './loadingSpinnerView';
-import {render as testRender} from '@testing-library/react-native';
+import { render as testRender } from '@testing-library/react-native';
 import React from 'react';
 
-const render = (props: any) =>
-  testRender(<LoadingSpinner testID="spinner" loading={props.loading} />);
+const render = (props: any) => testRender(<LoadingSpinner testID="spinner" />);
 
-it('renders and shows sign in text', () => {
-  const {getByTestId} = render({loading: false});
+it('renders', () => {
+  const { getByTestId } = render({ loading: false });
   expect(getByTestId('spinner')).toBeTruthy();
-});
-
-it('dissapears when loading is true', () => {
-  const {queryByTestId} = render({loading: true});
-  expect(queryByTestId('spinner')).toBeFalsy();
 });
