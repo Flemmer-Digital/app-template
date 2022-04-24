6735bc087468d065cd8421ed94a9b525c2ba8ec8
oliver flemmer <oli.flemmer@gmail.com>
2022-04-24 13:09:07 +0100

    write tests for pressableOpacity

--- app/src/components/pressableOpacity/pressableOpacity.test.tsx
+++ app/src/components/pressableOpacity/pressableOpacity.test.tsx
@@ -1,1 +1,17 @@
 import PressableOpacity from './pressableOpacityView';
+import { render as testRender } from '@testing-library/react-native';
+import { Text } from 'react-native';
+import React from 'react';
+import TextView from '../Text';
+
+const render = (props: any) =>
+  testRender(
+    <PressableOpacity style={props.styles}>
+      <TextView>pressableOpacity</TextView>
+    </PressableOpacity>,
+  );
+
+it('renders', () => {
+  const { getByText } = render({});
+  expect(getByText('pressableOpacity')).toBeTruthy();
+});
