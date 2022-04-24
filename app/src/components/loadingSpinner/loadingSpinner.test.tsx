67d81baa5525c5babceebc18a48c2660f5424acb
oliver flemmer <oli.flemmer@gmail.com>
2022-04-24 13:21:35 +0100

    unpack props in test

--- app/src/components/loadingSpinner/loadingSpinner.test.tsx
+++ app/src/components/loadingSpinner/loadingSpinner.test.tsx
@@ -2,7 +2,7 @@ import LoadingSpinner, { LoadingSpinnerProps } from './loadingSpinnerView';
 import { render as testRender } from '@testing-library/react-native';
 import React from 'react';
 
-const render = (props?: Partial<LoadingSpinnerProps>) => testRender(<LoadingSpinner />);
+const render = (props?: Partial<LoadingSpinnerProps>) => testRender(<LoadingSpinner {...props} />);
 
 it('renders', () => {
   const { getByTestId } = render();
