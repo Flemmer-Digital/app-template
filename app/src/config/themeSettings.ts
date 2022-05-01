a57d5ac836f8cd261ca6db70add4f0f3dd0baf8c
willflemmer <>
2022-05-01 12:27:39 +0100

    add themeSettings.ts

---
+++ app/src/config/themeSettings.ts
@@ -0,0 +1,12 @@
+interface IThemeSettings {
+  colors: {primary: string; secondary: string};
+}
+
+const themeSettings: IThemeSettings = {
+  colors: {
+    primary: '#eb4034',
+    secondary: '#3443eb',
+  },
+};
+
+export default themeSettings;
