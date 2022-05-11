7e5b28e7d50e2af2b337d8b76b1d5d4150f7dbda
oliver flemmer <oli.flemmer@gmail.com>
2022-05-11 14:39:27 +0100

    Create primary button jsx and update text props

--- app/src/components/button/primary/primary.tsx
+++ app/src/components/button/primary/primary.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import styles from './primaryStyles';
 import Button from '../baseButton';
+import {ViewStyle} from 'react-native';
 
 export interface PrimaryProps {
   text: string;
   onPress: () => void;
+  customStyle?: ViewStyle;
 }
 
-const Primary: React.FC<PrimaryProps> = ({text, onPress}) => {
-  return <Button text={text} onPress={onPress} />;
+const Primary: React.FC<PrimaryProps> = ({text, onPress, customStyle}) => {
+  return (
+    <Button
+      text={text}
+      onPress={onPress}
+      style={[styles.primary, customStyle]}
+      textStyle={styles.textStyle}
+    />
+  );
 };
 
 export default Primary;
--- app/src/components/button/primary/primaryStyles.ts
+++ app/src/components/button/primary/primaryStyles.ts
@@ -3,6 +3,14 @@ const {width, height} = Dimensions.get('window');
 export const vh = height / 100;
 export const vw = width / 100;
 
-const primaryStyles = StyleSheet.create({});
+const primaryStyles = StyleSheet.create({
+  textStyle: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  primary: {
+    backgroundColor: 'blue',
+  },
+});
 
 export default primaryStyles;
