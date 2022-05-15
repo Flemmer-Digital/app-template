e2763eda7a881958432b0ff7abaec2bd7efe8776
oliver flemmer <oli.flemmer@gmail.com>
2022-05-15 13:05:21 +0100

    remove onPress

--- app/src/components/button/baseButton/baseButton.test.tsx
+++ app/src/components/button/baseButton/baseButton.test.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Button, {ButtonProps} from './baseButton';
 import {render as testRender, fireEvent} from '@testing-library/react-native';
 
-const render = (props: Partial<ButtonProps>) =>
+const render = (props?: Partial<ButtonProps>) =>
   testRender(<Button text="Button" onPress={() => {}} {...props} />);
 
 it('renders correctly with text', () => {
-  const {getByText} = render({onPress: () => {}});
+  const {getByText} = render();
   expect(getByText('Button')).toBeTruthy();
 });
 
