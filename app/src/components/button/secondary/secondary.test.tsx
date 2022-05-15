6a88dacd6c1a69dd772612a5d50124c2396c9b11
oliver flemmer <oli.flemmer@gmail.com>
2022-05-15 12:32:58 +0100

    remove text from all testing props things

--- app/src/components/button/secondary/secondary.test.tsx
+++ app/src/components/button/secondary/secondary.test.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Secondary, {SecondaryProps} from './Secondary';
 import {render as testRender} from '@testing-library/react-native';
 
-const render = (props: Partial<SecondaryProps>) =>
+const render = (props?: Partial<SecondaryProps>) =>
   testRender(<Secondary text="text" onPress={() => {}} {...props} />);
 
 it('renders correctly', () => {
-  const {getByText} = render({text: 'text'});
+  const {getByText} = render();
   expect(getByText('text')).toBeTruthy();
 });
--- app/src/components/button/tertiary/tertiary.test.tsx
+++ app/src/components/button/tertiary/tertiary.test.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Tertiary, {TertiaryProps} from './tertiary';
 import {render as testRender} from '@testing-library/react-native';
 
-const render = (props: Partial<TertiaryProps>) =>
+const render = (props?: Partial<TertiaryProps>) =>
   testRender(<Tertiary text="text" onPress={() => {}} {...props} />);
 
 it('renders correctly', () => {
-  const {getByText} = render({text: 'text'});
+  const {getByText} = render();
   expect(getByText('text')).toBeTruthy();
 });
