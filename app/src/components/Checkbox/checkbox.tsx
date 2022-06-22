5f8d7c1bc658cb2b4998318c314facce0a1ae569
oliver flemmer <oli.flemmer@gmail.com>
2022-06-22 14:00:34 +0100

    create checkbox component

---
+++ app/src/components/Checkbox/checkbox.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './checkboxStyles';
+
+interface CheckboxProps {}
+
+const Checkbox: React.FC<CheckboxProps> = () => {
+  return <></>;
+};
+
+export default Checkbox;
