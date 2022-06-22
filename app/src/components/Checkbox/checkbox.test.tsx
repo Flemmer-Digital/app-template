a77b12e09e9d6e2d64bb9f7b9f5f252b713378b6
oliver flemmer <oli.flemmer@gmail.com>
2022-06-22 14:56:37 +0100

    forgot to commit in small steps

--- app/src/components/Checkbox/checkbox.test.tsx
+++ app/src/components/Checkbox/checkbox.test.tsx
@@ -1,2 +1,26 @@
-import Checkbox from './checkbox'
+import React from 'react';
+import Checkbox, { CheckboxProps } from './checkbox';
+import { render as testRender, fireEvent } from '@testing-library/react-native';
 
+const setChecked = jest.fn();
+
+const render = (props: Partial<CheckboxProps>) => {
+  return testRender(<Checkbox checked={false} label="checkbox" onPress={setChecked} {...props} />);
+};
+
+it('renders correctly', () => {
+  const { getByText } = render({});
+  expect(getByText('checkbox')).toBeTruthy();
+});
+
+it('runs onPress when pressed', () => {
+  const { getByText } = render({});
+  fireEvent.press(getByText('checkbox'));
+  expect(setChecked).toHaveBeenCalled();
+});
+
+it('does not run onPress if disabled', () => {
+  const { getByText } = render({ disabled: true });
+  fireEvent.press(getByText('checkbox'));
+  expect(setChecked).not.toHaveBeenCalled();
+});
--- app/src/components/Checkbox/checkbox.tsx
+++ app/src/components/Checkbox/checkbox.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import PressableOpacity from '../PressableOpacity';
 import styles from './checkboxStyles';
+import Flex from '../Flex/index';
+import Icon from 'react-native-vector-icons/Feather';
+import TextView from '../Text/index';
+import themeSettings from 'app/src/config/themeSettings';
 
-interface CheckboxProps {}
+export interface CheckboxProps {
+  checked: boolean;
+  label: React.ReactNode | string;
+  disabled?: boolean;
+  onPress: (currChecked: boolean) => void;
+}
 
-const Checkbox: React.FC<CheckboxProps> = () => {
-  return <></>;
+const Checkbox: React.FC<CheckboxProps> = ({ checked, label, disabled, onPress }) => {
+  return (
+    <PressableOpacity onPress={onPress} disabled={disabled} style={styles.container}>
+      <Flex
+        direction="row"
+        justifyContent="flex-start"
+        alignItems="center"
+        style={styles.flexContainer}
+      >
+        <Flex direction="row" justifyContent="center" alignItems="center" style={styles.box}>
+          {checked && <Icon name="check" size={20} color={themeSettings.colors.primary} />}
+        </Flex>
+        <TextView style={styles.label}>{label}</TextView>
+      </Flex>
+    </PressableOpacity>
+  );
 };
 
 export default Checkbox;
