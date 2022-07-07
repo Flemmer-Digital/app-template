67a12019b61c70ba4065d1032fc85af9e16980c2
oliver flemmer <oli.flemmer@gmail.com>
2022-07-07 11:09:23 +0100

    add more comments

--- app/scripts/updateExportComponents.py
+++ app/scripts/updateExportComponents.py
@@ -2,6 +2,8 @@ import os
 
 # Components with multiple components inside (e.g buttons)
 multiComponents = {'Button':[]}
+
+# Ignored since they are not standard components
 ignore = ['utils']
 
 # Create list of components excluding multiComponents
@@ -21,24 +23,28 @@ for key in multiComponents.keys():
       
   multiComponents[key] = sub_components
 
-
-import_string = 'import {0} from \'./{0}\';\n'
+# The template for to import/export components
+import_string = 'import {0} from \'./src/components/{0}\';\n'
 export_string = 'export {{ {0} }};\n'
-multi_import_string = 'import {0} from \'./{1}/{0}\';\n'
+multi_import_string = 'import {0} from \'./src/components/{1}/{0}\';\n'
 
-f = open('./src/components/exportComponents.ts', 'w')
+f = open('./exportComponents.ts', 'w')
 
+# Go through multi components adding imports
 print('\nMulti Components:')
 for master_component in multiComponents:
   for sub_component in multiComponents[master_component]:
     f.write(multi_import_string.format(sub_component, master_component))
     print('- ' + master_component + '/' + sub_component)
     
+  
+# Go through components adding imports
 print('\nSingle Components:')
 for component in components:
   f.write(import_string.format(component))
   print('- ' + component)
   
+# Add exports (not done in same loops as it wouldn'e result in the correct output)
 for component in components:
   f.write(export_string.format(component))
   
